72ecb6d59226b659298e0375561ea61ea237b24b
gurgeasson <hatizsak@gmail.com>
2023-06-16 22:52:33 +0100

    small chnage

--- randomEngine.py
+++ randomEngine.py
@@ -1,28 +1,26 @@
+#! /usr/bin/python
+
 # print('hello world')
 
-# load time module for sleep
 try:
-    import time
+    import time # load time module for sleep
 except RuntimeError:
     print("Error importing RPi.GPIO! This is probably because you need superuser privileges. Maybe try 'sudo'")
-# load RPi.GPIO module. I need it to have access to the GPIO pins.
 try:
-    import RPi.GPIO as GPIO
+    import RPi.GPIO as GPIO # load RPi.GPIO module. I need it to have access to the GPIO pins.
 except RuntimeError:
     print("Error importing RPi.GPIO! This is probably because you need superuser privileges. Maybe try 'sudo'")
-# I want to address the pins by the board number and not by the chip pin number.
-GPIO.setmode(GPIO.BOARD)
+
+GPIO.setmode(GPIO.BOARD) # I want to address the pins by the board number and not by the chip pin number.
 # print(GPIO.getmode())
 gpio_pin_7=7 # give a nive name to the GPIO pin the signal is attached to
-# set pin to accept input from radiation counter
-GPIO.setup(gpio_pin_7, GPIO.IN)
+GPIO.setup(gpio_pin_7, GPIO.IN) # set pin to accept input from radiation counter
 # print(GPIO.input(gpio_pin_7))
 
 while True:
     print('looping')
     time.sleep(3)
-
-
+    print(GPIO.input(gpio_pin_7))
 
 
 # GPIO.cleanup()
