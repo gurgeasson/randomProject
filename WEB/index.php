86d3aa3780e3eae0b15c4b0df948e45ba26b1781
gurgeasson <hatizsak@gmail.com>
2023-06-19 11:35:21 +0100

    change

--- WEB/index.php
+++ WEB/index.php
@@ -8,8 +8,7 @@
 </head>
 <body>
     <h1>Hello, world!</h1>   
-    
-    
+    <?php include_once 'list.html' ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
 </body>
 </html>
