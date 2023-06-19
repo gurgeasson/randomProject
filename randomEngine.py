ac922f751db62f95b6bd2280e8611175a2978b8d
gurgeasson <hatizsak@gmail.com>
2023-06-19 11:30:31 +0100

    change

--- randomEngine.py
+++ randomEngine.py
@@ -5,18 +5,18 @@
 try:
     import time # load time module for sleep
 except RuntimeError:
-    print("Error importing RPi.GPIO! This is probably because you need superuser privileges. Maybe try 'sudo'")
+    print("Error importing time! This is probably because you need superuser privileges. Maybe try 'sudo'")
 try:
     import RPi.GPIO as GPIO # load RPi.GPIO module. I need it to have access to the GPIO pins.
 except RuntimeError:
     print("Error importing RPi.GPIO! This is probably because you need superuser privileges. Maybe try 'sudo'")
 
-GPIO.setmode(GPIO.BCM) # I want to address the pins by the board number and not by the chip pin number.
+GPIO.setmode(GPIO.BCM) # I want to address the pins by the BCM number and not by the board number.
 # print(GPIO.getmode())
-gpio_my_pin = 4 # give a nive name to the GPIO pin the signal is attached to
-GPIO.setup(gpio_my_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set pin to accept input from radiation counter
+gpio_my_pin = 4 # creatr a variable to store the GPIO pin number that the signal is attached to
+GPIO.setup(gpio_my_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # set pin to accept input from radiation counter
 # print(GPIO.input(gpio_my_pin))
-print(GPIO.input(gpio_my_pin))
+
 while True:
     print('looping')
     time.sleep(1)
