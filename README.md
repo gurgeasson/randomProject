de3a1a922eb905e3dd2adbbb2e80b356adcc885c
gurgeasson <hatizsak@gmail.com>
2023-06-19 22:57:22 +0100

    change

--- README.md
+++ README.md
@@ -16,6 +16,8 @@ raspi pinout: https://pinout.xyz/pinout/pin7_gpio4</br>
 raspi gpio: https://www.makeuseof.com/tag/raspberry-pi-gpio-pins-guide/</br>
 https://sourceforge.net/p/raspberry-gpio-python/wiki/browse_pages/</br>
 nginx default www folder: https://serverfault.com/questions/718449/default-directory-for-nginx</br>
+python wait for edge: https://stackoverflow.com/questions/31303718/python-whats-the-most-efficient-way-to-wait-for-input</br>
+- and: http://raspi.tv/2013/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio</br>
 
 ha a geiger counter 5v-ot ad, akkot nem szabad hasznalnom a normal gpio pin-eket
 
--- randomEngine.py
+++ randomEngine.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
-
 # print('hello world')
+
 
 try:
     import time # load time module for sleep
@@ -13,25 +13,41 @@ except RuntimeError:
 
 GPIO.setmode(GPIO.BCM) # I want to address the pins by the BCM number and not by the board number.
 # print(GPIO.getmode())
-gpio_my_pin = 4 # creatr a variable to store the GPIO pin number that the signal is attached to
-GPIO.setup(gpio_my_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # set pin to accept input from radiation counter
+gpio_my_pin = 4 # create a variable to store the GPIO pin number that the signal is attached to
+GPIO.setup(gpio_my_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set pin to accept input from radiation counter
 # print(GPIO.input(gpio_my_pin))
 
 while True:
-    # print('looping')
-    # time.sleep(30)
-    # print(GPIO.input(gpio_my_pin))
+    try:
+        # wait for falling edge, store time in varable
+        # wait for rising edge, divide time from time variable...
+        # is this my random number? should I use it as a seed for shiftxor???
+        # store my random number in list.html
+        # sleep for about a minute??? half an hour??? to limit the number of random number entries 
+        GPIO.wait_for_edge(gpio_my_pin, GPIO.FALLING)
+        current_time = time.strftime("%H:%M:%S", time.localtime()) # generate time
+        file = open('/var/www/html/randomProject/list.html', 'at') # open list.html, 'at' - Append and Text mode
+        file.write(f'{GPIO.input(gpio_my_pin)} {current_time} </br>')
+        file.close()
+    except KeyboardInterrupt:
+        GPIO.cleanup()       # clean up GPIO on CTRL+C exit
+    GPIO.cleanup()           # clean up GPIO on normal exit
 
-    current_time = time.strftime("%H:%M:%S", time.localtime()) # generate time
+# while True:
+#     # print('looping')
+#     # time.sleep(30)
+#     # print(GPIO.input(gpio_my_pin))
 
-    # testing if I can add new entries as a list to my list.html.
-    # at the moment it is only the state of gpio_my_pin, and not a random number.
-    # later it will need to be database... actually just a text file.
-    # also I want the page automaticaly update every time I get a new random number.
-    file = open('/var/www/html/randomProject/list.html', 'at')
-    file.write(f'{GPIO.input(gpio_my_pin)} {current_time} </br>')
-    file.close()
+#     current_time = time.strftime("%H:%M:%S", time.localtime()) # generate time
+
+#     # testing if I can add new entries as a list to my list.html.
+#     # at the moment it is only the state of gpio_my_pin, and not a random number.
+#     # later it will need to be database... actually just a text file.
+#     # also I want the page automaticaly update every time I get a new random number.
+#     file = open('/var/www/html/randomProject/list.html', 'at')
+#     file.write(f'{GPIO.input(gpio_my_pin)} {current_time} </br>')
+#     file.close()
     
-    time.sleep(30)
+#     time.sleep(30)
 
 # GPIO.cleanup()
