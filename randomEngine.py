230fcbb2617478452c33f5f001654bf8c7974375
gurgeasson <hatizsak@gmail.com>
2023-06-23 12:13:20 +0100

    update

--- randomEngine.py
+++ randomEngine.py
@@ -1,26 +1,27 @@
 #! /usr/bin/python
 
-# in previous attampts it proved that I can connect to the radiation counter
-# I also proved that I can generate random numbers
-# now I want to see it I can do it with GPIO.wait_for_edge
-
-# I'm connecting to the INT2 pin on the radiation conunter.
+# This program interfaces with a radiation counter and generates random numbers from the detected signal.
+# I'm connecting to the INT2 pin on the radiation conunter. It's normaly low.
 # I'm using BOARD pin 12 on the raspberry pi
 # A logic level converter takes care of the 3.3v <--> 5v conversion between counter and pi
 
-import time
-import RPi.GPIO as GPIO
+# For now the random number is the elapsed time as a float between radiation hits in seconds
+# In future I would like to refine that
 
-gpio_my_pin = 12
+import time # needed for sleep and for timestamp
+import RPi.GPIO as GPIO # needed to access the GPIO pins
+
+gpio_my_pin = 12 # the GPIO pin I'm connecting to the counter
+# initialising wariables
 reference_hit = 0
 random_hit = 0
 
+# set up GPIO pins
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(gpio_my_pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN) # Input pin, accepts pullups from Radiation Counter.
 
-# random_engine function
-# I need a function to generate the random numbers
-
+# random_engine function --> generating true random numbers
+# checks if the reference_hit time stamp is within 5 seconds, mostly to keep the first generated number value within reason
 def random_engine():
     global reference_hit
     if (time.time() - 5) < reference_hit < time.time():
@@ -32,9 +33,9 @@ def random_engine():
         # print (f'{delta_time} : {time.strftime("%Y %b %d %H:%M:%S", time.gmtime())}') # prints the random number to the terminal
         current_time = time.strftime("%H:%M:%S", time.localtime()) # generate time
         file = open('/var/www/html/randomProject/list.html', 'at') # open list.html, 'at' - Append and Text mode
-        file.write(f'{delta_time} {current_time} </br>')
+        file.write(f'{delta_time} {current_time} </br>') # append random number and time it was created
         file.close()
-        time.sleep(5)
+        time.sleep(3600) # wait for an hour. It will yield about 24 numbers a day.
     else:
         # wait for up to 30 seconds for a rising edge (timeout is in milliseconds)
         GPIO.wait_for_edge(gpio_my_pin, GPIO.RISING, timeout=3000, bouncetime=200)
@@ -43,7 +44,7 @@ def random_engine():
 
 # main
 #
-# i'm not sure if I need any of the following any more
+# i'm not sure if I need any of the following any more, I could just wrap the functions code in a while loop...
 
 while True:
     random_engine()
--- test.py
+++ test.py
@@ -11,24 +11,17 @@ random_hit = 0
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(gpio_my_pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN) # Input pin, accepts pullups from Radiation Counter.
 
-while True: # While loop to keep the program running indefinitely.
-	if (time.time() - 3) < reference_hit < time.time(): # If my reference_hit time is not out of range
-		print('reference_hit in range')
-
-		while True:
-			do_things()
-
-		if GPIO.input(gpio_my_pin) == True: # If the geiger counter signal is high
-			random_hit = time.time() # Store the current time stamp
-			delta_time = random_hit - reference_hit # Calculate elapsed time from reference_hit to random_hit, and call taht my random number
-			# duration = str(duration)
-			# duration = duration.replace(".","") # removes the decimal point from the value
-			# duration = int(duration)
-			# duration = int(pow(duration, 20)) # Performs a raise on the value by a power of 20
-			print (f'{delta_time} : {time.strftime("%Y %b %d %H:%M:%S", time.gmtime())}') # prints the random number to the terminal
-			time.sleep(3) # I don't want too may numbers, wait for a min before the next one
-			# reference_hit = time.time()
-	else:
-		if GPIO.input(gpio_my_pin) == True: # AKA If the geiger counter signal is high
-			reference_hit = time.time()
-			print('set reference_hit')
+if GPIO.input(gpio_my_pin) == True: # If the geiger counter signal is high
+	random_hit = time.time() # Store the current time stamp
+	delta_time = random_hit - reference_hit # Calculate elapsed time from reference_hit to random_hit, and call taht my random number
+	# duration = str(duration)
+	# duration = duration.replace(".","") # removes the decimal point from the value
+	# duration = int(duration)
+	# duration = int(pow(duration, 20)) # Performs a raise on the value by a power of 20
+	print (f'{delta_time} : {time.strftime("%Y %b %d %H:%M:%S", time.gmtime())}') # prints the random number to the terminal
+	time.sleep(3) # I don't want too may numbers, wait for a min before the next one
+		# reference_hit = time.time()
+else:
+	if GPIO.input(gpio_my_pin) == True: # AKA If the geiger counter signal is high
+		reference_hit = time.time()
+		print('set reference_hit')
