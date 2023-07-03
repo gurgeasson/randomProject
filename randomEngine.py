ee5e2abccfb57ab29c241c358bb3be16cfeb5128
gurgeasson <hatizsak@gmail.com>
2023-07-03 15:35:03 +0100

    update

--- randomEngine.py
+++ randomEngine.py
@@ -12,7 +12,7 @@ import time # needed for sleep and for timestamp
 import RPi.GPIO as GPIO # needed to access the GPIO pins
 
 gpio_my_pin = 12 # the GPIO pin I'm connecting to the counter
-# initialising wariables
+# initialising variables
 reference_hit = 0
 random_hit = 0
 
@@ -24,18 +24,18 @@ GPIO.setup(gpio_my_pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN) # Input pin, acce
 # checks if the reference_hit time stamp is within 5 seconds, mostly to keep the first generated number value within reason
 def random_engine():
     global reference_hit
-    if (time.time() - 5) < reference_hit < time.time():
+    if (time.time() - 0) < reference_hit < time.time():
         # print('random_engine starts, does not much at this point')
-        # wait for up to 5 seconds for a rising edge (timeout is in milliseconds)
+        # wait for up to 30 seconds for a rising edge (timeout is in milliseconds)
         GPIO.wait_for_edge(gpio_my_pin, GPIO.RISING, timeout=3000, bouncetime=200)
         random_hit = time.time() # Store the current time stamp
         delta_time = random_hit - reference_hit # Calculate elapsed time from reference_hit to random_hit, and call taht my random number
         # print (f'{delta_time} : {time.strftime("%Y %b %d %H:%M:%S", time.gmtime())}') # prints the random number to the terminal
-        current_time = time.strftime("%H:%M:%S", time.localtime()) # generate time
+        current_time = time.strftime("%H:%M:%S - %Y/%b/%d", time.localtime()) # generate time
         file = open('/var/www/html/randomProject/list.html', 'at') # open list.html, 'at' - Append and Text mode
         file.write(f'{delta_time} {current_time} </br>') # append random number and time it was created
         file.close()
-        time.sleep(3600) # wait for an hour. It will yield about 24 numbers a day.
+        time.sleep(0) # don't wait, generate as much as possible (wait for an hour. It will yield about 24 numbers a day.)
     else:
         # wait for up to 30 seconds for a rising edge (timeout is in milliseconds)
         GPIO.wait_for_edge(gpio_my_pin, GPIO.RISING, timeout=3000, bouncetime=200)
